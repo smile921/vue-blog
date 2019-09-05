6cd4f6a885237d80ad0c02afd39e6cc912c7afd9
frere921 <cook@smile.org>
2019-09-05 13:52:31 +0800

    update vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,12 +16,12 @@ module.exports = {
   ],
   themeConfig: {
     serviceWorker: {
-      updatePopup: true // Boolean | Object, 默认值是 undefined.
+      updatePopup: true, // Boolean | Object, 默认值是 undefined.
       // 如果设置为 true, 默认的文本配置将是:
-      // updatePopup: {
-      //    message: "New content is available.",
-      //    buttonText: "Refresh"
-      // }
+      updatePopup: {
+        message: 'New content is available.',
+        buttonText: 'Refresh'
+      }
     },
     nav: [
       {
